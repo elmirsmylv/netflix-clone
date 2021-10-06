c75e00d32ac065057aba4867b9dd87ee53c14c1f
elmirsmylv <elmirsmylv@gmail.com>
2021-10-07 00:01:18 +0400

    some parts of landing page completed

---
+++ components/Header/Header.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import styles from "./Header.module.scss";
+import background from "../../public/background.jpg";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt="Netflix Logo"
+          className={styles.logo}
+        />
+        <div className={styles.nav_right}>
+          <select id="lang-switcher" className={styles.select}>
+            <option value="en" selected>
+              English
+            </option>
+            <option value="ru">Russian</option>
+          </select>
+          <button className={styles.button}>Sign In</button>
+        </div>
+      </nav>
+      <div className={styles.content}>
+        <div className={styles.img_wrapper}>
+          <img
+            className={styles.background_img}
+            src="./background.jpg"
+            alt="Netflix Background"
+          />
+          <div className={styles.img_gradient}></div>
+        </div>
+        <div className={styles.body}>
+          <h1>Unlimited movies, TV shows, and more.</h1>
+          <h3>Watch anywhere. Cancel anytime.</h3>
+          <span>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </span>
+          <div className={styles.input_section}>
+            <input type="text" placeholder="Email address" />
+            <button>Get Started</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/Section3/Section3.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./Section3.module.scss";
+
+interface Section1Props {
+  photo: string;
+  title: string;
+  video: string;
+  description: string;
+}
+
+//{ svg, video, title, description }: Section1Props
+
+const Section1 = ({ photo, video, title, description }: Section1Props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title_section}>
+        <h1>{title}</h1>
+        <h2>{description}</h2>
+      </div>
+      <div className={styles.animation_container}>
+        <img src={photo} />
+
+        <div className={styles.card_video}>
+          <video autoPlay playsInline muted loop>
+            <source src={video} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section1;
